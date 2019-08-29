ae8b6a3ee455901fcd530743138d65d575bffdd0
mkramers <mattkramers@gmail.com>
2019-08-28 20:26:15 -0400

    Add text utilities

---
+++ reqparser.common.tests/TextUtilitiesTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace reqparser.common.tests
+{
+    [TestFixture]
+    public class TextUtilitiesTests
+    {
+        [Test]
+        public void PrefixesTextCorrectly()
+        {
+            const string prefix = "\t";
+
+            string text = $"A{Environment.NewLine}\tB{Environment.NewLine}\t\tC";
+
+            string expectedPrefixedText = $"{prefix}A{Environment.NewLine}{prefix}\tB{Environment.NewLine}{prefix}\t\tC";
+            string actualPrefixedText = TextUtilities.PrefixTextLines(prefix, text);
+
+            Assert.That(actualPrefixedText, Is.EqualTo(expectedPrefixedText));
+        }
+    }
+}
---
+++ reqparser.common/TextUtilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace reqparser.common
+{
+    public static class TextUtilities
+    {
+        public static string PrefixTextLines(string _prefix, string _text)
+        {
+            StringBuilder prefixedText = new StringBuilder();
+
+            string[] lines = _text.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                string lineEnding = i < lines.Length - 1 ? Environment.NewLine : string.Empty;
+                prefixedText.Append($"{_prefix}{line}{lineEnding}");
+            }
+
+            return prefixedText.ToString();
+        }
+    }
+}
